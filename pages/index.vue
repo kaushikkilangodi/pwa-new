b3a2e40e8c26e6d07b150d728bc2cb0ac2999763
kaushikkilangodi1 <kaushikkilangodi@gmail.com>
2024-07-28 16:34:23 +0530

    updated

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,37 @@
 <template>
 <div>
-    <h1>Index page</h1>
-    <h2>updated</h2>
+    
+    <div class="container">
+    <h1>Todo List</h1>
+    <TodoInput />
+    <TodoList />
+  </div>
+   
     <PwaInstallButton/>
     <CameraAccess/>
     <Notification/>
 </div>
 </template>
+<script setup>
+import TodoInput from '~/components/TodoInput.vue'
+import TodoList from '~/components/TodoList.vue'
+</script>
+
+<style scoped>
+.container {
+  max-width: 600px;
+  margin: 50px auto;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  background-color: #fff;
+}
+
+h1 {
+  text-align: center;
+  margin-bottom: 30px;
+  font-size: 2em;
+  color: #333;
+}
+</style>
